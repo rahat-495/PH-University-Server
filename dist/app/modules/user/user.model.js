62208f1041f167873c61c75ba4a0fc9e3c0a6ec7
rahat-495 <md8702687@gmail.com>
2024-12-03 20:55:34 -0800

    dist folder are updated for user schema !

--- dist/app/modules/user/user.model.js
+++ dist/app/modules/user/user.model.js
@@ -1,1 +1,35 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UsersModel = void 0;
+const mongoose_1 = require("mongoose");
+const userSchema = new mongoose_1.Schema({
+    id: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    needsPasswordChange: {
+        type: Boolean,
+        default: true,
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ["in-progress", "blocked"],
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ["admin", "student", "faculty"],
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false,
+    },
+}, {
+    timestamps: true,
+});
+exports.UsersModel = (0, mongoose_1.model)('user', userSchema);
