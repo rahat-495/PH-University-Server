cf83be37d9397e558051ed196ef5e7a6b9fc874f
rahat-495 <md8702687@gmail.com>
2024-12-07 15:23:45 -0800

    send response utils are created !

--- src/app/modules/user/user.controllers.ts
+++ src/app/modules/user/user.controllers.ts
@@ -1,7 +1,8 @@
 
 import { NextFunction, Request, Response } from "express";
-import { userValidation } from "./user.validation";
 import { userService } from "./user.services";
+import sendResponse from "../../utils/sendResponse";
+import { TStudent } from "../student/student.interfaces";
 
 const createStudent = async (req : Request , res : Response , next : NextFunction) => {
     try {
@@ -9,11 +10,13 @@ const createStudent = async (req : Request , res : Response , next : NextFunctio
         const {password , student : studentData} = req.body ;
         // const zodParsedData = userValidation.userValidationSchema.parse(studentData) ;
         const result = await userService.createStudnetIntoDb(password  , studentData) ;
-        res.json({
-            success : true ,
-            message : "student created success fully !" ,
-            data : result ,
-        })
+        // res.json()
+        if(!result){
+            return ;
+        }
+        sendResponse<TStudent>(res , {success : true ,
+        message : "student created success fully !" , 
+        statusCode : 200 , data : result }) ;
 
     } catch (error) {
         next(error) ;
