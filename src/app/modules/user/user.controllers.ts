5fd06fe67998cb60fa4772764c476164248fe976
rahat-495 <md8702687@gmail.com>
2024-12-07 20:35:17 -0800

    catch async function are used in user creating controller !

--- src/app/modules/user/user.controllers.ts
+++ src/app/modules/user/user.controllers.ts
@@ -10,23 +10,17 @@ const catchAsync = (fn : RequestHandler) => {
     }
 } 
 
-const createStudent : RequestHandler = async (req , res , next) => {
-    try {
-        
-        const {password , student : studentData} = req.body ;
-        // const zodParsedData = userValidation.userValidationSchema.parse(studentData) ;
-        const result = await userService.createStudnetIntoDb(password  , studentData) ;
-        if(!result){
-            return ;
-        }
-        sendResponse<TStudent>(res , {success : true ,
-        message : "student created success fully !" , 
-        statusCode : 200 , data : result }) ;
-
-    } catch (error) {
-        next(error) ;
+const createStudent : RequestHandler = catchAsync( async (req , res , next) => { 
+    const {password , student : studentData} = req.body ;
+    // const zodParsedData = userValidation.userValidationSchema.parse(studentData) ;
+    const result = await userService.createStudnetIntoDb(password  , studentData) ;
+    if(!result){
+        return ;
     }
-}
+    sendResponse<TStudent>(res , {success : true ,
+    message : "student created success fully !" , 
+    statusCode : 200 , data : result }) ;
+}) ;
 
 export const userControllers = {
     createStudent ,
