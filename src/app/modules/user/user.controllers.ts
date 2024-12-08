de47bb9bf01cc4b9e25ef1c98ee863bce0d82129
rahat-495 <md8702687@gmail.com>
2024-12-07 20:13:16 -0800

    req type are now updated !

--- src/app/modules/user/user.controllers.ts
+++ src/app/modules/user/user.controllers.ts
@@ -1,16 +1,15 @@
 
-import { NextFunction, Request, Response } from "express";
+import { RequestHandler } from "express";
 import { userService } from "./user.services";
 import sendResponse from "../../utils/sendResponse";
 import { TStudent } from "../student/student.interfaces";
 
-const createStudent = async (req : Request , res : Response , next : NextFunction) => {
+const createStudent : RequestHandler = async (req , res , next) => {
     try {
         
         const {password , student : studentData} = req.body ;
         // const zodParsedData = userValidation.userValidationSchema.parse(studentData) ;
         const result = await userService.createStudnetIntoDb(password  , studentData) ;
-        // res.json()
         if(!result){
             return ;
         }
