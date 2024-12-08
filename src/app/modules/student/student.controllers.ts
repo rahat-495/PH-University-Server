1a80b4f8fcac01d0ee5e33708af14aa266da0f2d
rahat-495 <md8702687@gmail.com>
2024-12-07 20:22:18 -0800

    getting all students route are created !

--- src/app/modules/student/student.controllers.ts
+++ src/app/modules/student/student.controllers.ts
@@ -1,8 +1,15 @@
 
 import { RequestHandler } from "express"
+import { studentServices } from "./student.services";
+import sendResponse from "../../utils/sendResponse";
 
-const getAllStudents : RequestHandler = async (req , res) => {
-    
+const getAllStudents : RequestHandler = async (req , res , next) => {
+    try {
+        const result = await studentServices.getAllStudentsFromDb() ;
+        sendResponse<object>(res , {data : result , statusCode : 200 , success : true , message : "All students are retrived !"}) ;
+    } catch (error) {
+        next(error) ;
+    }
 }
 
 export const studentControllers = {
--- src/app/modules/student/student.services.ts
+++ src/app/modules/student/student.services.ts
@@ -1,2 +1,11 @@
 
+import { studentsModel } from "./student.model"
 
+const getAllStudentsFromDb = async () => {
+    const result = await studentsModel.find() ;
+    return result ;
+}
+
+export const studentServices = {
+    getAllStudentsFromDb ,
+}
