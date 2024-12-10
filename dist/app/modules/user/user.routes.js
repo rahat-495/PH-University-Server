1b10eb175df188beb155477891cff6d3402f3c36
rahat-495 <md8702687@gmail.com>
2024-12-09 20:59:26 -0800

    validateRequest function now are a seprete file for dist folder !

--- dist/app/modules/user/user.routes.js
+++ dist/app/modules/user/user.routes.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -16,17 +7,7 @@ exports.userRoutes = void 0;
 const express_1 = __importDefault(require("express"));
 const user_controllers_1 = require("./user.controllers");
 const student_validation_1 = require("../student/student.validation");
+const validateRequest_1 = __importDefault(require("../middlewares/validateRequest"));
 const router = express_1.default.Router();
-const validateRequest = (schema) => {
-    return (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-        try {
-            yield schema.parseAsync({ body: req.body });
-            return next();
-        }
-        catch (error) {
-            next(error);
-        }
-    });
-};
-router.post('/create-student', validateRequest(student_validation_1.studentValidationSchema), user_controllers_1.userControllers.createStudent);
+router.post('/create-student', (0, validateRequest_1.default)(student_validation_1.studentValidationSchema), user_controllers_1.userControllers.createStudent);
 exports.userRoutes = router;
