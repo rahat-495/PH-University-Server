d8ed9a5303b853dc7ebac4b2e9a76738ec00ef9a
rahat-495 <md8702687@gmail.com>
2024-12-09 20:58:56 -0800

    validateRequest function now are a seprete file !

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,21 +1,10 @@
 
-import express, { NextFunction, Request, Response } from "express"
+import express from "express"
 import { userControllers } from "./user.controllers";
-import { AnyZodObject } from "zod";
 import { studentValidationSchema } from "../student/student.validation";
+import validateRequest from "../middlewares/validateRequest";
 
 const router = express.Router() ;
-
-const validateRequest = (schema : AnyZodObject) => {
-    return async (req : Request , res : Response , next : NextFunction) => {
-        try {
-            await schema.parseAsync({body : req.body});
-            return next() ;
-        } catch (error) {
-            next(error) ;
-        }
-    }
-}
 
 router.post('/create-student' , validateRequest(studentValidationSchema) , userControllers.createStudent) ;
 
