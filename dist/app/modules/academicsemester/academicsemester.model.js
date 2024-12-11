937d79a2598a396c0eda2ad1e50edc5078b72c80
rahat-495 <md8702687@gmail.com>
2024-12-10 21:02:20 -0800

    academicSemester model are done for dist folder !

--- dist/app/modules/academicsemester/academicsemester.model.js
+++ dist/app/modules/academicsemester/academicsemester.model.js
@@ -1,1 +1,32 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const academicSemesterSchema = new mongoose_1.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    year: {
+        type: Date,
+        required: true,
+    },
+    code: {
+        type: String,
+        required: true,
+    },
+    startMonth: {
+        enum: months,
+        type: String,
+        required: true,
+    },
+    endMonth: {
+        enum: months,
+        type: String,
+        required: true,
+    },
+}, {
+    timestamps: true,
+});
+const academicSemestersModel = (0, mongoose_1.model)("academicSemester", academicSemesterSchema);
+exports.default = academicSemestersModel;
