878f70194bb35dc68f84a94d6ef72a8167a2c47c
rahat-495 <md8702687@gmail.com>
2024-12-11 21:38:39 -0800

    createacademicSemesterIntoDb are now valideted success fully !

--- src/app/modules/academicsemester/academicsemester.services.ts
+++ src/app/modules/academicsemester/academicsemester.services.ts
@@ -1,11 +1,19 @@
 
-import config from "../../config";
-import { TStudent } from "../student/student.interfaces";
-import { studentsModel } from "../student/student.model";
 import { TAcademicSemester } from "./academicSemester.interface";
 import academicSemestersModel from "./academicSemester.model";
 
 const createacademicSemesterIntoDb = async (data : TAcademicSemester) => {
+    type TAcademicSemesterNameCodeMapper = {
+        [keys : string] : string ;
+    } ;
+    const academicSemesterNameCodeMapper : TAcademicSemesterNameCodeMapper = {
+        Autumn : "01" ,
+        Summer : "02" ,
+        Fall : "03" ,
+    }
+    if(academicSemesterNameCodeMapper[data?.name] !== data?.code){
+        throw new Error("Invalid Semester Code !") ;
+    }
     const newAcademicSemester = await academicSemestersModel.create(data) ;
     return newAcademicSemester ;
 }
