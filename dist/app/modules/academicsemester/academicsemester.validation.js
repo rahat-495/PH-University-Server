6195fb95b8ac6a32b50019b58f600c831d48b713
rahat-495 <md8702687@gmail.com>
2024-12-14 21:18:22 -0800

    academic semester validation are done for dist folder !

--- dist/app/modules/academicsemester/academicsemester.validation.js
+++ dist/app/modules/academicsemester/academicsemester.validation.js
@@ -12,6 +12,16 @@ const createAcademicSemesterValidationSchema = zod_1.z.object({
         endMonth: zod_1.z.enum([...academicSemester_constant_1.months]),
     }),
 });
+const updateAcademicSemesterValidationSchema = zod_1.z.object({
+    body: zod_1.z.object({
+        name: zod_1.z.enum([...academicSemester_constant_1.academicSemesterName]).optional(),
+        year: zod_1.z.string().optional(),
+        code: zod_1.z.enum([...academicSemester_constant_1.academicSemesterCode]).optional(),
+        startMonth: zod_1.z.enum([...academicSemester_constant_1.months]).optional(),
+        endMonth: zod_1.z.enum([...academicSemester_constant_1.months]).optional(),
+    }),
+});
 exports.academicSemesterValidations = {
     createAcademicSemesterValidationSchema,
+    updateAcademicSemesterValidationSchema,
 };
