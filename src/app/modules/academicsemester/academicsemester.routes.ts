45c45416dad8b23390ae9e3929e7e00ae3e6763d
rahat-495 <md8702687@gmail.com>
2024-12-14 21:18:05 -0800

    academic semester validation are done !

--- src/app/modules/academicsemester/academicsemester.routes.ts
+++ src/app/modules/academicsemester/academicsemester.routes.ts
@@ -9,6 +9,6 @@ const router = Router() ;
 router.post('/create-academic-semester' , validateRequest(academicSemesterValidations.createAcademicSemesterValidationSchema) , academicSemesterControllers.createAcademicSemester) ;
 router.get('/get-all-academic-semester' , academicSemesterControllers.getAllAcademicSemester) ;
 router.get('/get-academic-semester/:semesterId' , academicSemesterControllers.getAcademicSemester) ;
-router.patch('/update-academic-semester/:semesterId' , academicSemesterControllers.updateAcademicSemester) ;
+router.patch('/update-academic-semester/:semesterId' , validateRequest(academicSemesterValidations.updateAcademicSemesterValidationSchema) , academicSemesterControllers.updateAcademicSemester) ;
 
 export const academicSemesterRoutes = router ;
--- src/app/modules/academicsemester/academicsemester.validation.ts
+++ src/app/modules/academicsemester/academicsemester.validation.ts
@@ -1,5 +1,5 @@
 
-import {string, z} from 'zod' ;
+import {z} from 'zod' ;
 import { academicSemesterCode, academicSemesterName, months } from './academicSemester.constant';
 
 const createAcademicSemesterValidationSchema = z.object({
@@ -12,6 +12,17 @@ const createAcademicSemesterValidationSchema = z.object({
     }),
 });
 
+const updateAcademicSemesterValidationSchema = z.object({
+    body : z.object({
+        name : z.enum([...academicSemesterName] as [string , ...string[]]).optional() ,
+        year : z.string().optional() ,
+        code : z.enum([...academicSemesterCode] as [string , ...string[]]).optional() ,
+        startMonth : z.enum([...months] as [string , ...string[]]).optional() ,
+        endMonth : z.enum([...months] as [string , ...string[]]).optional() ,
+    }),
+});
+
 export const academicSemesterValidations = {
     createAcademicSemesterValidationSchema ,
+    updateAcademicSemesterValidationSchema ,
 }
