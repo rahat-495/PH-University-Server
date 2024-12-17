bc63fa49c69f2caa1513706348dbe8485491f216
rahat-495 <md8702687@gmail.com>
2024-12-17 15:02:29 -0800

    academicFaculty service file are done !

--- src/app/modules/academicFaculty/academicFaculty.services.ts
+++ src/app/modules/academicFaculty/academicFaculty.services.ts
@@ -1,2 +1,30 @@
 
+import { TAcademicFaculty } from "./academicFaculty.interfaces";
+import { academicFacultysModel } from "./academicFaculty.model";
 
+const createAacademicFacultyIntoDb = async (payload : TAcademicFaculty) => {
+    const result = await academicFacultysModel.create(payload) ;
+    return result ;
+}
+
+const getAllAcademicFacultiesFromDb = async () => {
+    const result = await academicFacultysModel.find() ;
+    return result ;
+}
+
+const getSpecificAcademicFacultyFromDb = async (id : string) => {
+    const result = await academicFacultysModel.findById(id) ;
+    return result ;
+}
+
+const updateAAcademicFacultyIntoDb = async (id : string , payload : TAcademicFaculty) => {
+    const result = await academicFacultysModel.updateOne({_id : id} , {$set : payload}) ;
+    return result ;
+}
+
+export const studentServices = {
+    createAacademicFacultyIntoDb,
+    updateAAcademicFacultyIntoDb ,
+    getAllAcademicFacultiesFromDb ,
+    getSpecificAcademicFacultyFromDb ,
+}
