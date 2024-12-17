824611a41d37ea168638c56a5d4ce06d15687fbb
rahat-495 <md8702687@gmail.com>
2024-12-16 21:04:32 -0800

    creating custom generated id is fully done !

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -4,11 +4,22 @@ import { UsersModel } from "./user.model";
 
 const findLastUserId = async () => {
     const lastUserId = await UsersModel.findOne({role : "student"} , {id : 1 , _id : 0}).sort({createdAt : -1}).lean() ;
-    return lastUserId?.id ? lastUserId.id.substring(6) : undefined ;
+    return lastUserId?.id ? lastUserId.id : undefined ;
 }
 
 export const generateStudentId = async (payload : TAcademicSemester) => {
-    const currentId = await findLastUserId() || (0).toString() ;
+    let currentId = (0).toString() ;
+    const lastUserId = await findLastUserId() ;
+    
+    const lastStudentyear = lastUserId?.substring(0,4) ;
+    const lastStudentSemesterCode = lastUserId?.substring(4,6) ;
+    const currnetYear = payload.year ;
+    const currnetCode = payload.code ;
+
+    if(lastUserId && lastStudentSemesterCode === currnetCode && lastStudentyear === currnetYear){
+        currentId = lastUserId.substring(6) ;
+    }
+
     let incrementId = (Number(currentId)+1).toString().padStart(4,'0') ;
     incrementId = `${payload?.year}${payload?.code}${incrementId}` ;
     return incrementId ;
