fa166d8b0687a8b0e11ce9774b7ccc6b44784b6b
rahat-495 <md8702687@gmail.com>
2024-12-17 14:46:48 -0800

    academicFaculty model are fully done for dist folder !

--- dist/app/modules/academicFaculty/academicFaculty.model.js
+++ dist/app/modules/academicFaculty/academicFaculty.model.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.academicFacultysModel = void 0;
 const mongoose_1 = require("mongoose");
 const academicFacultySchema = new mongoose_1.Schema({
     name: {
@@ -7,3 +8,4 @@ const academicFacultySchema = new mongoose_1.Schema({
         required: [true, "Enter the name !"]
     }
 });
+exports.academicFacultysModel = (0, mongoose_1.model)("academicFaculty", academicFacultySchema);
