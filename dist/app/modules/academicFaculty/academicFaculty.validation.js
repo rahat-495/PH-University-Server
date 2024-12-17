aa7e62fab1b22f47b5c724561f0beb4768ef9b40
rahat-495 <md8702687@gmail.com>
2024-12-17 14:55:10 -0800

    academicFaculty validation schema some thing are updated for dist folder !

--- dist/app/modules/academicFaculty/academicFaculty.validation.js
+++ dist/app/modules/academicFaculty/academicFaculty.validation.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.academicFacultyValidation = void 0;
 const zod_1 = require("zod");
 const academicFacultyValidationSchema = zod_1.z.object({
-    name: zod_1.z.string({ invalid_type_error: "name must be string !" }),
+    name: zod_1.z.string({ invalid_type_error: "Academic faculty name must be string !" }),
 });
 exports.academicFacultyValidation = {
     academicFacultyValidationSchema,
