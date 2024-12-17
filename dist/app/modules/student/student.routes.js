39aa75e28bf3747be5f063ddce5343479c1acbea
rahat-495 <md8702687@gmail.com>
2024-12-17 14:34:26 -0800

    one student route path are changed for dist folder !

--- dist/app/modules/student/student.routes.js
+++ dist/app/modules/student/student.routes.js
@@ -7,7 +7,7 @@ exports.studentRoutes = void 0;
 const express_1 = __importDefault(require("express"));
 const student_controllers_1 = require("./student.controllers");
 const router = express_1.default.Router();
-router.get('/students', student_controllers_1.studentControllers.getAllStudents);
+router.get('/', student_controllers_1.studentControllers.getAllStudents);
 router.get('/:id', student_controllers_1.studentControllers.getSpecificStudent);
 router.get('/:id', student_controllers_1.studentControllers.deleteAStudent);
 exports.studentRoutes = router;
