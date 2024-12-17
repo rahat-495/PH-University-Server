972b502ee7c7267032e3bafb07c8880d5714a0e5
rahat-495 <md8702687@gmail.com>
2024-12-17 14:53:37 -0800

    academicFaculty validation schema are done !

--- src/app/modules/academicFaculty/academicFaculty.validation.ts
+++ src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -1,2 +1,10 @@
 
+import {z} from 'zod' ;
 
+const academicFacultyValidationSchema = z.object({
+    name : z.string({invalid_type_error : "name must be string !"}) ,
+});
+
+export const academicFacultyValidation = {
+    academicFacultyValidationSchema ,
+}
