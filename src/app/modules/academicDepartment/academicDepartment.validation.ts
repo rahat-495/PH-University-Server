769da59684365da27215a998828030367b42c258
rahat-495 <md8702687@gmail.com>
2024-12-19 11:56:34 +0600

    academic department some things are updated !

--- src/app/modules/academicDepartment/academicDepartment.validation.ts
+++ src/app/modules/academicDepartment/academicDepartment.validation.ts
@@ -10,8 +10,8 @@ const createAcademicDepartmentValidationSchema = z.object({
 
 const updateAcademicDepartmentValidationSchema = z.object({
     body : z.object({
-        name : z.string({invalid_type_error : "Academic Department name must be string !" , required_error : "name is required !"}) ,
-        academicFaculty : z.string({invalid_type_error : "Academic faculty must be string !" , required_error : "academic faculty is required !"}) ,
+        name : z.string({invalid_type_error : "Academic Department name must be string !" , required_error : "name is required !"}).optional() ,
+        academicFaculty : z.string({invalid_type_error : "Academic faculty must be string !" , required_error : "academic faculty is required !"}).optional() ,
     })
 });
 
