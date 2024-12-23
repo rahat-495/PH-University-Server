d08ac4a37de4f92543c1270c6c387b3139f3edeb
rahat-495 <md8702687@gmail.com>
2024-12-23 20:49:25 +0600

    session transaction are done for user creating for dist folder !

--- dist/app/modules/user/user.services.js
+++ dist/app/modules/user/user.services.js
@@ -13,26 +13,40 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.userService = void 0;
+const mongoose_1 = __importDefault(require("mongoose"));
 const config_1 = __importDefault(require("../../config"));
 const academicSemester_model_1 = __importDefault(require("../academicSemester/academicSemester.model"));
 const student_model_1 = require("../student/student.model");
 const user_model_1 = require("./user.model");
 const user_utils_1 = require("./user.utils");
+const AppErrors_1 = __importDefault(require("../../errors/AppErrors"));
 const createStudnetIntoDb = (password, studentData) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a, _b;
     const userData = {};
     userData.role = 'student';
     const academicDetails = yield academicSemester_model_1.default.findById(studentData.admissionSemester);
-    if (!academicDetails) {
-        return false;
-    }
-    userData.id = yield (0, user_utils_1.generateStudentId)(academicDetails);
-    userData.password = password || config_1.default.defaultPass;
-    const newUser = yield user_model_1.UsersModel.create(userData);
-    if (newUser === null || newUser === void 0 ? void 0 : newUser._id) {
-        studentData.id = newUser === null || newUser === void 0 ? void 0 : newUser.id;
-        studentData.user = newUser === null || newUser === void 0 ? void 0 : newUser._id;
-        const newStudent = yield student_model_1.studentsModel.create(studentData);
+    const session = yield mongoose_1.default.startSession();
+    try {
+        session.startTransaction();
+        userData.id = yield (0, user_utils_1.generateStudentId)(academicDetails);
+        userData.password = password || config_1.default.defaultPass;
+        const newUser = yield user_model_1.UsersModel.create([userData], { session });
+        if (!(newUser === null || newUser === void 0 ? void 0 : newUser.length)) {
+            throw new AppErrors_1.default(500, 'Failed to create user');
+        }
+        studentData.id = (_a = newUser[0]) === null || _a === void 0 ? void 0 : _a.id;
+        studentData.user = (_b = newUser[0]) === null || _b === void 0 ? void 0 : _b._id;
+        const newStudent = yield student_model_1.studentsModel.create([studentData], { session });
+        if (!(newStudent === null || newStudent === void 0 ? void 0 : newStudent.length)) {
+            throw new AppErrors_1.default(500, 'Failed to create student');
+        }
+        yield session.commitTransaction();
+        yield session.endSession();
         return newStudent;
+    }
+    catch (error) {
+        yield session.abortTransaction();
+        yield session.endSession();
     }
 });
 exports.userService = {
