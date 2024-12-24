b88a1df5790ae55eda98484be0b1e82237476793
rahat-495 <md8702687@gmail.com>
2024-12-24 20:44:10 +0600

    studentu update validation schema are done !

--- src/app/modules/student/student.routes.ts
+++ src/app/modules/student/student.routes.ts
@@ -1,12 +1,14 @@
 
 import express from "express"
 import { studentControllers } from "./student.controllers";
+import validateRequest from "../middlewares/validateRequest";
+import { studentValidations } from "./student.validation";
 
 const router = express.Router() ;
 
 router.get('/' , studentControllers.getAllStudents) ;
 router.get('/:id' , studentControllers.getSpecificStudent) ;
-router.patch('/:id' , studentControllers.updateAStudent) ;
+router.patch('/:id' , validateRequest(studentValidations.updateStudentValidationSchema) , studentControllers.updateAStudent) ;
 router.delete('/:id' , studentControllers.deleteAStudent) ;
 
 export const studentRoutes = router ;
