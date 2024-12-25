a6ff6a81d9d4145a061136767a0d4394b50db5bc
rahat-495 <md8702687@gmail.com>
2024-12-25 14:57:08 +0600

    updating student details now updated for dist folder !

--- dist/app/modules/student/student.services.js
+++ dist/app/modules/student/student.services.js
@@ -8,6 +8,17 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -26,7 +37,25 @@ const getSpecificStudentFromDb = (id) => __awaiter(void 0, void 0, void 0, funct
     return result;
 });
 const updateAStudentIntoDb = (id, payload) => __awaiter(void 0, void 0, void 0, function* () {
-    const result = yield student_model_1.studentsModel.findOneAndUpdate({ id }, { $set: payload }, { new: true });
+    const { name, guardian, localGuardian } = payload, remainingStudentData = __rest(payload, ["name", "guardian", "localGuardian"]);
+    const modifiedUpdateData = Object.assign({}, remainingStudentData);
+    if (name && Object.keys(name).length) {
+        for (const [key, value] of Object.entries(name)) {
+            modifiedUpdateData[`name.${key}`] = value;
+        }
+    }
+    if (guardian && Object.keys(guardian).length) {
+        for (const [key, value] of Object.entries(guardian)) {
+            modifiedUpdateData[`guardian.${key}`] = value;
+        }
+    }
+    if (localGuardian && Object.keys(localGuardian).length) {
+        for (const [key, value] of Object.entries(localGuardian)) {
+            modifiedUpdateData[`localGuardian.${key}`] = value;
+        }
+    }
+    console.log(modifiedUpdateData);
+    const result = yield student_model_1.studentsModel.findOneAndUpdate({ id }, modifiedUpdateData, { new: true, runValidators: true });
     return result;
 });
 const deleteAStudentFromDb = (id) => __awaiter(void 0, void 0, void 0, function* () {
