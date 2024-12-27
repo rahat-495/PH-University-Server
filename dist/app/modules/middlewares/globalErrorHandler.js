2aa8ed56c6042f28d63508da6e1e223fe407922b
rahat-495 <md8702687@gmail.com>
2024-12-27 20:51:49 +0600

    handling mongoose validation error are done for dist folder !

--- dist/app/modules/middlewares/globalErrorHandler.js
+++ dist/app/modules/middlewares/globalErrorHandler.js
@@ -6,12 +6,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const zod_1 = require("zod");
 const config_1 = __importDefault(require("../../config"));
 const HandleZodError_1 = __importDefault(require("../../errors/HandleZodError"));
+const HandleValidationError_1 = __importDefault(require("../../errors/HandleValidationError"));
 const globalErrorHandler = (err, req, res, next) => {
     let statusCode = err.statusCode || 500;
     let message = err.message || "some thing wen't wrong";
     let errorSources = [{ path: "", message: "Some thing went wrong" }];
     if (err instanceof zod_1.ZodError) {
         const simplifiedError = (0, HandleZodError_1.default)(err);
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message;
+        errorSources = simplifiedError.errorSources;
+    }
+    else if (err.name === "ValidationError") {
+        const simplifiedError = (0, HandleValidationError_1.default)(err);
         statusCode = simplifiedError.statusCode;
         message = simplifiedError.message;
         errorSources = simplifiedError.errorSources;
