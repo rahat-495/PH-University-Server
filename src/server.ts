81c8fdf2d3fcb065236c9bcae7b2f69659f81279
rahat-495 <md8702687@gmail.com>
2024-12-28 12:04:00 +0600

    unhandledRejection are done !

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,16 @@
 import mongoose from "mongoose";
 import app from "./app"
 import config from "./app/config";
+import {Server} from "http";
 
+let server : Server ;
 const main = async () => {
 
     try {
 
         await mongoose.connect(config.databaseUrl as string) ;
     
-        app.listen(config.port , () => {
+        server = app.listen(config.port , () => {
             console.log(`server are running at port ${config.port} !`) ;
         })
         
@@ -20,3 +22,12 @@ const main = async () => {
 }
 
 main() ;
+
+process.on("unhandledRejection" , () => {
+    if(server){
+        server.close(() => {
+            process.exit(1) ;
+        })
+    }
+    process.exit(1) ;   
+})
