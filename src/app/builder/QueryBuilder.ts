a843f5157c82d599b3cd3ad0c6faaaf48a864fb6
rahat-495 <md8702687@gmail.com>
2024-12-29 14:57:48 +0600

    searching are done for class !

--- src/app/builder/QueryBuilder.ts
+++ src/app/builder/QueryBuilder.ts
@@ -1,5 +1,5 @@
 
-import { Query } from "mongoose";
+import { FilterQuery, Query } from "mongoose";
 
 class QueryBuilder<T> {
     public modelQuery : Query<T[] , T> ;
@@ -8,5 +8,20 @@ class QueryBuilder<T> {
     constructor(modelQuery : Query<T[] , T> , query : Record<string , unknown>){
         this.modelQuery = modelQuery ;
         this.query = query ;
+    };
+
+    search(searchAbleFields : string[]){
+        let searchTerm = "" ;
+        if(this?.query?.searchTerm){
+            searchTerm = this?.query?.searchTerm as string ;
+        }
+        if(this?.query?.searchTerm){
+            this.modelQuery = this.modelQuery.find({
+                $or : searchAbleFields.map((field) => ({ [field] : {$regex : searchTerm , $options : "i"} }) as FilterQuery<T>)
+            }) ;
+        }
+        return this ;
     }
 }
+
+export default QueryBuilder ;
