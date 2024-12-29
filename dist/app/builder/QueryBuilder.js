a6e9147d3d0548522dd14dd199d659be63e3b767
rahat-495 <md8702687@gmail.com>
2024-12-29 14:42:45 +0600

    query builder class are created for dist folder !

---
+++ dist/app/builder/QueryBuilder.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class QueryBuilder {
+    constructor(modelQuery, query) {
+        this.modelQuery = modelQuery;
+        this.query = query;
+    }
+}
