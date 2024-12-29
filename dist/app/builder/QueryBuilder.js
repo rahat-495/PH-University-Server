764a394535b7952b9963f1823c9e1a613c53a629
rahat-495 <md8702687@gmail.com>
2024-12-29 15:22:17 +0600

    paginate are done at class at dist folder !

--- dist/app/builder/QueryBuilder.js
+++ dist/app/builder/QueryBuilder.js
@@ -28,5 +28,13 @@ class QueryBuilder {
         this.modelQuery = this.modelQuery.sort(sort);
         return this;
     }
+    paginate() {
+        var _a, _b;
+        const page = Number((_a = this === null || this === void 0 ? void 0 : this.query) === null || _a === void 0 ? void 0 : _a.page) || 1;
+        const limit = Number((_b = this === null || this === void 0 ? void 0 : this.query) === null || _b === void 0 ? void 0 : _b.limit) || 10;
+        const skip = (page - 1) * limit;
+        this.modelQuery = this.modelQuery.skip(skip).limit(limit);
+        return this;
+    }
 }
 exports.default = QueryBuilder;
