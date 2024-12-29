6f49441ed08d1603806c5d2f79b1dc06b36ab157
rahat-495 <md8702687@gmail.com>
2024-12-29 14:58:03 +0600

    searching are done for class for dist folder !

--- dist/app/builder/QueryBuilder.js
+++ dist/app/builder/QueryBuilder.js
@@ -5,4 +5,19 @@ class QueryBuilder {
         this.modelQuery = modelQuery;
         this.query = query;
     }
+    ;
+    search(searchAbleFields) {
+        var _a, _b, _c;
+        let searchTerm = "";
+        if ((_a = this === null || this === void 0 ? void 0 : this.query) === null || _a === void 0 ? void 0 : _a.searchTerm) {
+            searchTerm = (_b = this === null || this === void 0 ? void 0 : this.query) === null || _b === void 0 ? void 0 : _b.searchTerm;
+        }
+        if ((_c = this === null || this === void 0 ? void 0 : this.query) === null || _c === void 0 ? void 0 : _c.searchTerm) {
+            this.modelQuery = this.modelQuery.find({
+                $or: searchAbleFields.map((field) => ({ [field]: { $regex: searchTerm, $options: "i" } }))
+            });
+        }
+        return this;
+    }
 }
+exports.default = QueryBuilder;
