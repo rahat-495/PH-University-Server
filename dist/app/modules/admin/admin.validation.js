5ac84a4fb86b1ae042c7d17d0edf0748e4b266c2
rahat-495 <md8702687@gmail.com>
2025-01-01 21:28:07 +0600

    admin validation are done at dist folder !

--- dist/app/modules/admin/admin.validation.js
+++ dist/app/modules/admin/admin.validation.js
@@ -1,1 +1,57 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.adminValidations = void 0;
+const zod_1 = require("zod");
+const createUserNameValidationSchema = zod_1.z.object({
+    firstName: zod_1.z.string().max(20).min(1).refine((value) => /^[A-Z]/.test(value), { message: "first name start with capital letter !" }),
+    middleName: zod_1.z.string(),
+    lastName: zod_1.z.string(),
+});
+const createAdminValidationSchema = zod_1.z.object({
+    body: zod_1.z.object({
+        password: zod_1.z.string().max(20),
+        faculty: zod_1.z.object({
+            name: createUserNameValidationSchema,
+            gender: zod_1.z.enum(["male", "female", "other"]),
+            dateOfBirth: zod_1.z.string(),
+            email: zod_1.z.string().email(),
+            contactNo: zod_1.z.string(),
+            emergencyContactNo: zod_1.z.string(),
+            presentAddress: zod_1.z.string(),
+            permanentAddress: zod_1.z.string(),
+            profileImg: zod_1.z.string(),
+            isActive: zod_1.z.enum(["active", "blocked"]).default("active"),
+            isDeleted: zod_1.z.boolean().optional(),
+            managementDepartment: zod_1.z.string(),
+            designation: zod_1.z.string(),
+        })
+    }),
+});
+const updateUserNameValidationSchema = zod_1.z.object({
+    firstName: zod_1.z.string().max(20).min(1).refine((value) => /^[A-Z]/.test(value), { message: "first name start with capital letter !" }).optional(),
+    middleName: zod_1.z.string().optional(),
+    lastName: zod_1.z.string().optional(),
+});
+const updateAdminValidationSchema = zod_1.z.object({
+    body: zod_1.z.object({
+        faculty: zod_1.z.object({
+            name: updateUserNameValidationSchema.optional(),
+            gender: zod_1.z.enum(["male", "female", "other"]).optional(),
+            dateOfBirth: zod_1.z.string().optional(),
+            email: zod_1.z.string().email().optional(),
+            contactNo: zod_1.z.string().optional(),
+            emergencyContactNo: zod_1.z.string().optional(),
+            presentAddress: zod_1.z.string().optional(),
+            permanentAddress: zod_1.z.string().optional(),
+            profileImg: zod_1.z.string().optional(),
+            isActive: zod_1.z.enum(["active", "blocked"]).default("active").optional(),
+            isDeleted: zod_1.z.boolean().optional().optional(),
+            managementDepartment: zod_1.z.string().optional(),
+            designation: zod_1.z.string().optional(),
+        })
+    }),
+});
+exports.adminValidations = {
+    createAdminValidationSchema,
+    updateAdminValidationSchema,
+};
