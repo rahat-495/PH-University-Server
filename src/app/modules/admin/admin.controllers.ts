d5952d21590b0728aed33c723e277af146b0f241
rahat-495 <md8702687@gmail.com>
2025-01-01 21:12:17 +0600

    admin controllers are done !

---
+++ src/app/modules/admin/admin.controllers.ts
@@ -0,0 +1,43 @@
+
+import { RequestHandler } from "express"
+import sendResponse from "../../utils/sendResponse";
+import catchAsync from "../../utils/catchAsync";
+
+const getAllAdmin : RequestHandler = catchAsync(async (req , res , next) => {
+    const result = await adminServices.getAllAdminFromDb(req.query) ;
+    sendResponse<object>(res , {data : result , statusCode : 200 , success : true , message : "All faculties are retrived !"}) ;
+}) ;
+
+const getSpecifiAdmin : RequestHandler = catchAsync(async (req , res , next) => {
+    const {adminId} = req.params ;
+    const data = await adminServices.getSpecificAdminFromDb(adminId) ;
+    if(data){
+        sendResponse<object>(res , {data , statusCode : 200 , success : true , message : "Specific faculties are retrived !"}) ;
+    }
+    else{
+        sendResponse<object>(res , {data : {} , statusCode : 200 , success : true , message : "Can't Get Any admin !"}) ;
+    }
+}) ;
+
+const updateAdmin : RequestHandler = catchAsync(async (req , res , next) => {
+    const {adminId} = req.params ;
+    const result = await adminServices.updateSingleAdminIntoDb(adminId , req.body.admin) ;
+    if(result){
+        sendResponse<object>(res , {data : result , statusCode : 200 , success : true , message : "admin details udpated success fully !"}) ;
+    }
+}) ;
+
+const deleteAdmin : RequestHandler = catchAsync(async (req , res , next) => {
+    const {adminId} = req.params ;
+    const result = await adminServices.deleteSingleAdminFromDb(adminId) ;
+    if(result){
+        sendResponse<object>(res , {data : result , statusCode : 200 , success : true , message : "Specific admin are deleted !"}) ;
+    }
+}) ;
+
+export const adminControllers = {
+    getAllAdmin,
+    deleteAdmin ,
+    updateAdmin ,
+    getSpecifiAdmin ,
+}
--- src/app/modules/admin/admin.routes.ts
+++ src/app/modules/admin/admin.routes.ts
@@ -1,12 +1,13 @@
 
 import express from "express"
 import validateRequest from "../middlewares/validateRequest";
+import { adminControllers } from "./admin.controllers";
 
 const router = express.Router() ;
 
 router.get('/' , adminControllers.getAllAdmin) ;
 router.get('/:adminId' , adminControllers.getSpecifiAdmin) ;
-router.patch('/:adminId' , validateRequest(adminValidations.updateAdminValidationSchema) , adminControllers.updateAdmin) ;
+router.patch('/:adminId' , adminControllers.updateAdmin) ;
 router.delete('/:adminId' , adminControllers.deleteAdmin) ;
 
 export const adminRoutes = router ;
