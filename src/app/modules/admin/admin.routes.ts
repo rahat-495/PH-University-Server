868c3e484d7fd4051f9a9f9fc9aa41a8377b8e5d
rahat-495 <md8702687@gmail.com>
2025-01-01 21:29:15 +0600

    the validation are called at the admin route !

--- src/app/modules/admin/admin.routes.ts
+++ src/app/modules/admin/admin.routes.ts
@@ -2,12 +2,13 @@
 import express from "express"
 import validateRequest from "../middlewares/validateRequest";
 import { adminControllers } from "./admin.controllers";
+import { adminValidations } from "./admin.validation";
 
 const router = express.Router() ;
 
 router.get('/' , adminControllers.getAllAdmin) ;
 router.get('/:adminId' , adminControllers.getSpecifiAdmin) ;
-router.patch('/:adminId' , adminControllers.updateAdmin) ;
+router.patch('/:adminId'  , validateRequest(adminValidations.updateAdminValidationSchema) , adminControllers.updateAdmin) ;
 router.delete('/:adminId' , adminControllers.deleteAdmin) ;
 
 export const adminRoutes = router ;
