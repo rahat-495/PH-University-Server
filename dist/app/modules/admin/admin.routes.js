8dd467de461276b0d62a12f071bf7ddf5776eb80
rahat-495 <md8702687@gmail.com>
2025-01-01 21:29:29 +0600

    the validation are called at the admin route at dist folder !

--- dist/app/modules/admin/admin.routes.js
+++ dist/app/modules/admin/admin.routes.js
@@ -5,10 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.adminRoutes = void 0;
 const express_1 = __importDefault(require("express"));
+const validateRequest_1 = __importDefault(require("../middlewares/validateRequest"));
 const admin_controllers_1 = require("./admin.controllers");
+const admin_validation_1 = require("./admin.validation");
 const router = express_1.default.Router();
 router.get('/', admin_controllers_1.adminControllers.getAllAdmin);
 router.get('/:adminId', admin_controllers_1.adminControllers.getSpecifiAdmin);
-router.patch('/:adminId', admin_controllers_1.adminControllers.updateAdmin);
+router.patch('/:adminId', (0, validateRequest_1.default)(admin_validation_1.adminValidations.updateAdminValidationSchema), admin_controllers_1.adminControllers.updateAdmin);
 router.delete('/:adminId', admin_controllers_1.adminControllers.deleteAdmin);
 exports.adminRoutes = router;
