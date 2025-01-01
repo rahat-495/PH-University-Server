7004c7d3994b1b4337ea7d505e5b2c5c93c6d493
rahat-495 <md8702687@gmail.com>
2025-01-01 21:00:11 +0600

    some things are changed @

--- src/app/modules/student/student.controllers.ts
+++ src/app/modules/student/student.controllers.ts
@@ -3,7 +3,6 @@ import { RequestHandler } from "express"
 import { studentServices } from "./student.services";
 import sendResponse from "../../utils/sendResponse";
 import catchAsync from "../../utils/catchAsync";
-import { TStudent } from "./student.interfaces";
 
 const getAllStudents : RequestHandler = catchAsync(async (req , res , next) => {
     const result = await studentServices.getAllStudentsFromDb(req.query) ;
