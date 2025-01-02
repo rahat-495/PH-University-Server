441f45b424a4866e8d4a7b5ef8c085ce61fd6da4
rahat-495 <md8702687@gmail.com>
2025-01-02 22:00:57 +0600

    creating course service are done !

---
+++ src/app/modules/course/course.services.ts
@@ -0,0 +1,12 @@
+
+import { TCourse } from "./course.interfaces";
+import { coursesModel } from "./course.model";
+
+const createCourseIntoDb = async (payload: TCourse) => {
+    const result = await coursesModel.create(payload) ;
+    return result ;
+}
+
+export const courseServices = {
+    createCourseIntoDb ,
+}
