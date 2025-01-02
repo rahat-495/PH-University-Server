06f816266af20cf751b36ed41db5456faf42655c
rahat-495 <md8702687@gmail.com>
2025-01-02 21:02:39 +0600

    faculty constant filtering name added at dist folder !

--- dist/app/modules/faculty/faculty.constand.js
+++ dist/app/modules/faculty/faculty.constand.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.facultiesSearchAbleFields = void 0;
-exports.facultiesSearchAbleFields = ["name.firstName", "email"];
+exports.facultiesSearchAbleFields = ["name.firstName", "name.middleName", "name.lastName", "id", "contactNo", "emergencyContactNo", "email"];
