f4d3fc612d3dd86a25b08bab3e76d0d552d2fdac
rahat-495 <md8702687@gmail.com>
2025-01-02 21:52:48 +0600

    course schema are done at dist folder !

--- dist/app/modules/course/course.model.js
+++ dist/app/modules/course/course.model.js
@@ -1,1 +1,31 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const preRequisiteCoursesSchema = new mongoose_1.Schema({
+    course: { type: mongoose_1.Schema.Types.ObjectId },
+    isDeleter: { type: Boolean, default: false },
+});
+const courseSchema = new mongoose_1.Schema({
+    title: {
+        trim: true,
+        type: String,
+        unique: true,
+        required: [true, "Title is required !"],
+    },
+    prefix: {
+        trim: true,
+        type: String,
+        required: [true, "prefix is required !"],
+    },
+    code: {
+        trim: true,
+        type: Number,
+        required: [true, "code is required !"],
+    },
+    credits: {
+        trim: true,
+        type: Number,
+        required: [true, "credits is required !"],
+    },
+    preRequisiteCourses: [preRequisiteCoursesSchema],
+});
