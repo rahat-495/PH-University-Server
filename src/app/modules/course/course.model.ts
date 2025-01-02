c72e76d6be6f6765108276a10dcda7bffc8706a9
rahat-495 <md8702687@gmail.com>
2025-01-02 21:52:28 +0600

    course schema are done !

---
+++ src/app/modules/course/course.model.ts
@@ -0,0 +1,33 @@
+
+import { Schema } from "mongoose";
+import { TCourse, TPreRequisiteCourse } from "./course.interfaces";
+
+const preRequisiteCoursesSchema = new Schema<TPreRequisiteCourse>({
+    course : { type : Schema.Types.ObjectId } ,
+    isDeleter : { type : Boolean , default : false } ,
+})
+
+const courseSchema = new Schema<TCourse>({
+    title : {
+        trim : true ,
+        type : String ,
+        unique : true ,
+        required : [true , "Title is required !"] ,
+    },
+    prefix : {
+        trim : true ,
+        type : String ,
+        required : [true , "prefix is required !"] ,
+    },
+    code : {
+        trim : true ,
+        type : Number ,
+        required : [true , "code is required !"] ,
+    },
+    credits : {
+        trim : true ,
+        type : Number ,
+        required : [true , "credits is required !"] ,
+    },
+    preRequisiteCourses : [preRequisiteCoursesSchema] ,
+})
