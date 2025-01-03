fb2d97d6d9a765294655fc8a3fb3b50bafa754c7
rahat-495 <md8702687@gmail.com>
2025-01-03 21:06:11 +0600

    validating the create course route are done at dist folder !

--- dist/app/modules/course/course.routes.js
+++ dist/app/modules/course/course.routes.js
@@ -1,8 +1,13 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.courseRoutes = void 0;
 const express_1 = require("express");
 const course_controllers_1 = require("./course.controllers");
+const validateRequest_1 = __importDefault(require("../middlewares/validateRequest"));
+const course_validation_1 = require("./course.validation");
 const router = (0, express_1.Router)();
-router.post("/create", course_controllers_1.courseControllers.createCourse);
+router.post("/create", (0, validateRequest_1.default)(course_validation_1.courseValidations.createCourseValidation), course_controllers_1.courseControllers.createCourse);
 exports.courseRoutes = router;
