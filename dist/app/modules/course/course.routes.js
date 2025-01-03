4961005083f29bba71cf2c4b3296488068c7f1a5
rahat-495 <md8702687@gmail.com>
2025-01-03 21:00:38 +0600

    creating course route are added at dist folder !

--- dist/app/modules/course/course.routes.js
+++ dist/app/modules/course/course.routes.js
@@ -2,5 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.courseRoutes = void 0;
 const express_1 = require("express");
+const course_controllers_1 = require("./course.controllers");
 const router = (0, express_1.Router)();
+router.post("/create", course_controllers_1.courseControllers.createCourse);
 exports.courseRoutes = router;
