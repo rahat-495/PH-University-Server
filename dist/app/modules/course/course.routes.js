3f7e44becd408beed5f3d29729ec61024048efa3
rahat-495 <md8702687@gmail.com>
2025-01-03 21:07:45 +0600

    create course name are changed at dist folder !

--- dist/app/modules/course/course.routes.js
+++ dist/app/modules/course/course.routes.js
@@ -9,5 +9,5 @@ const course_controllers_1 = require("./course.controllers");
 const validateRequest_1 = __importDefault(require("../middlewares/validateRequest"));
 const course_validation_1 = require("./course.validation");
 const router = (0, express_1.Router)();
-router.post("/create", (0, validateRequest_1.default)(course_validation_1.courseValidations.createCourseValidation), course_controllers_1.courseControllers.createCourse);
+router.post("/create-course", (0, validateRequest_1.default)(course_validation_1.courseValidations.createCourseValidation), course_controllers_1.courseControllers.createCourse);
 exports.courseRoutes = router;
