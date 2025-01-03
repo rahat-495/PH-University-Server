9411c0a6e4ff68b4300d944e7c35bfc597a6b49c
rahat-495 <md8702687@gmail.com>
2025-01-03 20:54:46 +0600

    creating course controller are done at dist folder !

--- dist/app/modules/course/course.controllers.js
+++ dist/app/modules/course/course.controllers.js
@@ -1,1 +1,23 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const course_services_1 = require("./course.services");
+const sendResponse_1 = __importDefault(require("../../utils/sendResponse"));
+const createCourse = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield course_services_1.courseServices.createCourseIntoDb(req.body);
+    (0, sendResponse_1.default)(res, { data: result, statusCode: 200, success: true, message: "Course Created Successfully !" });
+});
+const courseControllers = {
+    createCourse,
+};
