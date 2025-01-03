1943c8b490b61355a0d3eba7c5c281f6d1a5ed1b
rahat-495 <md8702687@gmail.com>
2025-01-03 21:05:49 +0600

    validating the create course route are done !

--- src/app/modules/course/course.routes.ts
+++ src/app/modules/course/course.routes.ts
@@ -1,9 +1,11 @@
 
 import { Router } from "express";
 import { courseControllers } from "./course.controllers";
+import validateRequest from "../middlewares/validateRequest";
+import { courseValidations } from "./course.validation";
 
 const router = Router() ;
 
-router.post("/create" , courseControllers.createCourse) ;
+router.post("/create" , validateRequest(courseValidations.createCourseValidation) , courseControllers.createCourse) ;
 
 export const courseRoutes = router ;
