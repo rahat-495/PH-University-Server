94609e92764e13cf49e5f0bb116445adf5be0983
rahat-495 <md8702687@gmail.com>
2025-01-03 21:04:53 +0600

    course validation schema are done !

---
+++ src/app/modules/course/course.validation.ts
@@ -0,0 +1,21 @@
+
+import { z } from "zod";
+
+const preRequisiteCoursevalidationSchema = z.object({
+    course : z.string() ,
+    isDeleted : z.boolean().optional() ,
+})
+
+const createCourseValidation = z.object({
+    body : z.object({
+        title : z.string() ,
+        prefix : z.string() ,
+        code : z.number() ,
+        credits : z.number() , 
+        preRequisiteCourses : z.array(preRequisiteCoursevalidationSchema) ,
+    })
+})
+
+export const courseValidations = {
+    createCourseValidation ,
+}
