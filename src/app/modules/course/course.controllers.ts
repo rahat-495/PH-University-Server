883b0b385521dd27bd65122c017043484f6c63a7
rahat-495 <md8702687@gmail.com>
2025-01-03 20:59:14 +0600

    deleteCourse controller are done 1

--- src/app/modules/course/course.controllers.ts
+++ src/app/modules/course/course.controllers.ts
@@ -18,7 +18,15 @@ const getSingleCourse : RequestHandler = async (req , res) => {
     sendResponse<object>(res , {data : result , statusCode : 200 , success : true , message : "Courses are retrive Successfully !"}) ;
 }
 
+const deleteCourse : RequestHandler = async (req , res) => {
+    const result = await courseServices.deleteCourseIntoDb(req.params.id) ;
+    if(result){
+        sendResponse<object>(res , {data : result , statusCode : 200 , success : true , message : "Courses are deleted Successfully !"}) ;
+    }
+}
+
 export const courseControllers = {
+    deleteCourse,
     createCourse ,
     getAllCourses ,
     getSingleCourse ,
