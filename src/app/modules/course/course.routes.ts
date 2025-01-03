157cd11cbecbf3e5eb1ab950b6a77238ed076287
rahat-495 <md8702687@gmail.com>
2025-01-03 21:12:36 +0600

    getting all course route are added

--- src/app/modules/course/course.routes.ts
+++ src/app/modules/course/course.routes.ts
@@ -7,5 +7,6 @@ import { courseValidations } from "./course.validation";
 const router = Router() ;
 
 router.post("/create-course" , validateRequest(courseValidations.createCourseValidation) , courseControllers.createCourse) ;
+router.get("/" , courseControllers.getAllCourses) ;
 
 export const courseRoutes = router ;
