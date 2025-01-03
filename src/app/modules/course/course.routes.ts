3dbe3580a570c61d09593f549e4e86f20e1dda1a
rahat-495 <md8702687@gmail.com>
2025-01-03 21:00:24 +0600

    creating course route are added !

--- src/app/modules/course/course.routes.ts
+++ src/app/modules/course/course.routes.ts
@@ -1,6 +1,9 @@
 
 import { Router } from "express";
+import { courseControllers } from "./course.controllers";
 
 const router = Router() ;
+
+router.post("/create" , courseControllers.createCourse) ;
 
 export const courseRoutes = router ;
