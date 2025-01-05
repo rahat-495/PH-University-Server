16bc085d801350250ed380e5ad3ed1e28f74178f
rahat-495 <md8702687@gmail.com>
2025-01-05 21:39:25 +0600

    update course service is on work at dist folder !

--- dist/app/modules/course/course.services.js
+++ dist/app/modules/course/course.services.js
@@ -8,6 +8,17 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -30,8 +41,9 @@ const getSingleCourseFromDb = (id) => __awaiter(void 0, void 0, void 0, function
     return result;
 });
 const updateCourseIntoDb = (id, payload) => __awaiter(void 0, void 0, void 0, function* () {
-    const result = yield course_model_1.coursesModel.findByIdAndUpdate(id, { isDeleted: true }, { new: true });
-    return result;
+    const { preRequisiteCourses } = payload, courseRemainingData = __rest(payload, ["preRequisiteCourses"]);
+    const updateBasicCourseIntoDb = yield course_model_1.coursesModel.findByIdAndUpdate(id, courseRemainingData, { new: true, runValidators: true });
+    return updateBasicCourseIntoDb;
 });
 const deleteCourseIntoDb = (id) => __awaiter(void 0, void 0, void 0, function* () {
     const result = yield course_model_1.coursesModel.findByIdAndUpdate(id, { isDeleted: true }, { new: true });
