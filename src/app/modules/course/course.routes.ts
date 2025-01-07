9145fcc95f050955a6211e532e21007463fd0a53
rahat-495 <md8702687@gmail.com>
2025-01-07 22:36:59 +0600

    some bug are fixed !

--- src/app/modules/course/course.routes.ts
+++ src/app/modules/course/course.routes.ts
@@ -11,7 +11,7 @@ router.get("/:id" , courseControllers.getSingleCourse) ;
 router.delete("/:id" , courseControllers.deleteCourse) ;
 router.patch("/:id" , validateRequest(courseValidations.updateCourseValidation) , courseControllers.updateCourse) ;
 router.post("/create-course" , validateRequest(courseValidations.createCourseValidation) , courseControllers.createCourse) ;
-router.put('/:courseId/remove-faculties' , validateRequest(courseValidations.facultiesWithCourseValidationSchema) , courseControllers.removeFacultiesWithCourse) ;
-router.delete('/:courseId/assign-faculties' , validateRequest(courseValidations.facultiesWithCourseValidationSchema) , courseControllers.assignFacultiesWithCourse) ;
+router.put('/:courseId/assign-faculties' , validateRequest(courseValidations.facultiesWithCourseValidationSchema) , courseControllers.assignFacultiesWithCourse) ;
+router.delete('/:courseId/remove-faculties' , validateRequest(courseValidations.facultiesWithCourseValidationSchema) , courseControllers.removeFacultiesWithCourse) ;
 
 export const courseRoutes = router ;
