cf7df996d62e2f7b3f06086443a170984cce42d0
rahat-495 <md8702687@gmail.com>
2025-01-08 11:24:10 +0600

    validatin schema adding routes are done !

--- src/app/modules/semesterRegistration/semesterRegistration.routes.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.routes.ts
@@ -1,12 +1,14 @@
 
 import { Router } from "express";
 import { semesterRegistrationControllers } from "./semesterRegistration.controllers";
+import validateRequest from "../middlewares/validateRequest";
+import { semesterRegistrationValidations } from "./semesterRegistration.validation";
 
 const router = Router() ;
 
 router.get('/' , semesterRegistrationControllers.getAllSemesterRegistration) ;
-router.patch('/:id' , semesterRegistrationControllers.updateSemesterRegistration) ;
 router.get('/:id' , semesterRegistrationControllers.getSingleSemesterRegistration) ;
-router.post('/create-semester-registration' , semesterRegistrationControllers.createSemesterRegistration) ;
+router.patch('/:id' , validateRequest(semesterRegistrationValidations.updateSemesterRegistrationValidationSchema) , semesterRegistrationControllers.updateSemesterRegistration) ;
+router.post('/create-semester-registration' , validateRequest(semesterRegistrationValidations.createSemesterRegistrationValidationSchema) , semesterRegistrationControllers.createSemesterRegistration) ;
 
 export const semesterRegistrationRoutes = router ;
