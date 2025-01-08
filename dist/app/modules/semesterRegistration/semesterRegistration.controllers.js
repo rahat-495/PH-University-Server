285735b15292caf686e98eb653832975539aa7ae
rahat-495 <md8702687@gmail.com>
2025-01-08 11:18:00 +0600

    semester registration services are added at dist folder !

--- dist/app/modules/semesterRegistration/semesterRegistration.controllers.js
+++ dist/app/modules/semesterRegistration/semesterRegistration.controllers.js
@@ -1,1 +1,48 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.semesterRegistrationControllers = void 0;
+const sendResponse_1 = __importDefault(require("../../utils/sendResponse"));
+const catchAsync_1 = __importDefault(require("../../utils/catchAsync"));
+const semesterRegistration_services_1 = require("./semesterRegistration.services");
+const createSemesterRegistration = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield semesterRegistration_services_1.semesterRegistrationServices.createSemesterRegistrationIntoDb(req.body);
+    if (result) {
+        (0, sendResponse_1.default)(res, { data: result, statusCode: 200, success: true, message: "Semester Registration Created Successfully !" });
+    }
+}));
+const getAllSemesterRegistration = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield semesterRegistration_services_1.semesterRegistrationServices.getAllSemesterRegistrationFromDb(req.query);
+    if (result) {
+        (0, sendResponse_1.default)(res, { data: result, statusCode: 200, success: true, message: "All Semester Registration are retrive Successfully !" });
+    }
+}));
+const getSingleSemesterRegistration = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield semesterRegistration_services_1.semesterRegistrationServices.getSingleSemesterRegistrationFromDb(req.params.id);
+    if (result) {
+        (0, sendResponse_1.default)(res, { data: result, statusCode: 200, success: true, message: "Semester Registration are retrive Successfully !" });
+    }
+}));
+const updateSemesterRegistration = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield semesterRegistration_services_1.semesterRegistrationServices.updateSemesterRegistrationIntoDb(req.params.id, req.body);
+    if (result) {
+        (0, sendResponse_1.default)(res, { data: result, statusCode: 200, success: true, message: "Semester Registration are updated Successfully !" });
+    }
+}));
+exports.semesterRegistrationControllers = {
+    updateSemesterRegistration,
+    createSemesterRegistration,
+    getAllSemesterRegistration,
+    getSingleSemesterRegistration,
+};
--- dist/app/modules/semesterRegistration/semesterRegistration.services.js
+++ dist/app/modules/semesterRegistration/semesterRegistration.services.js
@@ -1,1 +1,40 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.semesterRegistrationServices = void 0;
+const QueryBuilder_1 = __importDefault(require("../../builder/QueryBuilder"));
+const semesterRegistration_constant_1 = require("./semesterRegistration.constant");
+const semesterRegistration_model_1 = require("./semesterRegistration.model");
+const createSemesterRegistrationIntoDb = (payload) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield semesterRegistration_model_1.semesterRegistrationsModel.create(payload);
+    return result;
+});
+const getAllSemesterRegistrationFromDb = (query) => __awaiter(void 0, void 0, void 0, function* () {
+    const courseQuery = new QueryBuilder_1.default(semesterRegistration_model_1.semesterRegistrationsModel.find().populate("preRequisiteCourses.course"), query).search(semesterRegistration_constant_1.semesterRegistrationSearchAbleFields).filter().sort().paginate().fields();
+    const result = yield courseQuery.modelQuery;
+    return result;
+});
+const getSingleSemesterRegistrationFromDb = (id) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield semesterRegistration_model_1.semesterRegistrationsModel.findById(id);
+    return result;
+});
+const updateSemesterRegistrationIntoDb = (id, payload) => __awaiter(void 0, void 0, void 0, function* () {
+    return {};
+});
+exports.semesterRegistrationServices = {
+    createSemesterRegistrationIntoDb,
+    getAllSemesterRegistrationFromDb,
+    updateSemesterRegistrationIntoDb,
+    getSingleSemesterRegistrationFromDb,
+};
