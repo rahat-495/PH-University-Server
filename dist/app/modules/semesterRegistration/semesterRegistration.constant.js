beb0bf9e701884bdc448f7c37ccc041a05b24c9f
rahat-495 <md8702687@gmail.com>
2025-01-08 11:40:38 +0600

    semester registration schema are done at dist folder !

--- dist/app/modules/semesterRegistration/semesterRegistration.constant.js
+++ dist/app/modules/semesterRegistration/semesterRegistration.constant.js
@@ -1,4 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.semesterRegistrationSearchAbleFields = void 0;
+exports.semesterRegistrationStatus = exports.semesterRegistrationSearchAbleFields = void 0;
 exports.semesterRegistrationSearchAbleFields = ["", ""];
+exports.semesterRegistrationStatus = ['UPCOMING', 'ONGOING', 'ENDED'];
--- dist/app/modules/semesterRegistration/semesterRegistration.model.js
+++ dist/app/modules/semesterRegistration/semesterRegistration.model.js
@@ -2,5 +2,36 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.semesterRegistrationsModel = void 0;
 const mongoose_1 = require("mongoose");
-const semesterRegistrationSchema = new mongoose_1.Schema({});
+const semesterRegistration_constant_1 = require("./semesterRegistration.constant");
+const semesterRegistrationSchema = new mongoose_1.Schema({
+    academicSemester: {
+        type: mongoose_1.Schema.Types.ObjectId,
+        ref: "academicSemester",
+        required: true,
+        unique: true,
+    },
+    status: {
+        type: String,
+        default: "UPCOMING",
+        enum: semesterRegistration_constant_1.semesterRegistrationStatus,
+    },
+    startDate: {
+        type: Date,
+        required: true,
+    },
+    endDate: {
+        type: Date,
+        required: true,
+    },
+    minCredit: {
+        type: Number,
+        default: 3,
+    },
+    maxCredit: {
+        type: Number,
+        default: 15,
+    },
+}, {
+    timestamps: true,
+});
 exports.semesterRegistrationsModel = (0, mongoose_1.model)("semesterRegistration", semesterRegistrationSchema);
