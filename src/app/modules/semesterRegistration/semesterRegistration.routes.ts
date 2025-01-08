ae0be4174db06ca7c06bebf9f68663f43f9f0ea8
rahat-495 <md8702687@gmail.com>
2025-01-08 11:02:15 +0600

    semesterRegistration some routes are creatd !

--- src/app/modules/semesterRegistration/semesterRegistration.routes.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.routes.ts
@@ -3,6 +3,9 @@ import { Router } from "express";
 
 const router = Router() ;
 
+router.get('/') ;
+router.get('/:id') ;
+router.patch('/:id') ;
 router.post('/create-semester-registration') ;
 
 export const semesterRegistrationRoutes = router ;
