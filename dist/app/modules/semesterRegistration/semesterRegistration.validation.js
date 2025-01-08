61b3fbf34708fd05b89cdd83510eb7c40d61057a
rahat-495 <md8702687@gmail.com>
2025-01-08 11:22:52 +0600

    semester registration validation schema are added at dist folder !

--- dist/app/modules/semesterRegistration/semesterRegistration.validation.js
+++ dist/app/modules/semesterRegistration/semesterRegistration.validation.js
@@ -1,1 +1,14 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.semesterRegistrationValidations = void 0;
+const zod_1 = require("zod");
+const createSemesterRegistrationValidationSchema = zod_1.z.object({
+    body: zod_1.z.object({}),
+});
+const updateSemesterRegistrationValidationSchema = zod_1.z.object({
+    body: zod_1.z.object({}),
+});
+exports.semesterRegistrationValidations = {
+    createSemesterRegistrationValidationSchema,
+    updateSemesterRegistrationValidationSchema,
+};
