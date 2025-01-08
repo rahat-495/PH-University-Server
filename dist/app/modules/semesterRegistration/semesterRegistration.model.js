db5090440542b3e23780893ec3910c353c7e7a5b
rahat-495 <md8702687@gmail.com>
2025-01-08 11:07:55 +0600

    semesterRegistration model are created at dist folder !

--- dist/app/modules/semesterRegistration/semesterRegistration.model.js
+++ dist/app/modules/semesterRegistration/semesterRegistration.model.js
@@ -1,1 +1,6 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.semesterRegistrationsModel = void 0;
+const mongoose_1 = require("mongoose");
+const semesterRegistrationSchema = new mongoose_1.Schema({});
+exports.semesterRegistrationsModel = (0, mongoose_1.model)("semesterRegistration", semesterRegistrationSchema);
