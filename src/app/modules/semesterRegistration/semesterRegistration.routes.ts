148f299d960d25d3ca1405cd0139a0e8591e2ec2
rahat-495 <md8702687@gmail.com>
2025-01-08 11:19:33 +0600

    controllers are added at semester registration routes !

--- src/app/modules/semesterRegistration/semesterRegistration.routes.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.routes.ts
@@ -1,11 +1,12 @@
 
 import { Router } from "express";
+import { semesterRegistrationControllers } from "./semesterRegistration.controllers";
 
 const router = Router() ;
 
-router.get('/') ;
-router.get('/:id') ;
-router.patch('/:id') ;
-router.post('/create-semester-registration') ;
+router.get('/' , semesterRegistrationControllers.getAllSemesterRegistration) ;
+router.patch('/:id' , semesterRegistrationControllers.updateSemesterRegistration) ;
+router.get('/:id' , semesterRegistrationControllers.getSingleSemesterRegistration) ;
+router.post('/create-semester-registration' , semesterRegistrationControllers.createSemesterRegistration) ;
 
 export const semesterRegistrationRoutes = router ;
