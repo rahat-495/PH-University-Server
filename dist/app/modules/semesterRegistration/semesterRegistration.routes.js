e8322dfb92517990524eb84ca3d7463917aba1f4
rahat-495 <md8702687@gmail.com>
2025-01-08 11:01:04 +0600

    creating semeter registration route are created at dist folder !

--- dist/app/modules/semesterRegistration/semesterRegistration.routes.js
+++ dist/app/modules/semesterRegistration/semesterRegistration.routes.js
@@ -1,1 +1,7 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.semesterRegistrationRoutes = void 0;
+const express_1 = require("express");
+const router = (0, express_1.Router)();
+router.post('/create-semester-registration');
+exports.semesterRegistrationRoutes = router;
