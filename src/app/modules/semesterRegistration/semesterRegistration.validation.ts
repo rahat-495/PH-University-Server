35ff0db0af926ce1f5b58341023a271944ed7bfc
rahat-495 <md8702687@gmail.com>
2025-01-08 11:45:24 +0600

    creating semeter registration validation schema are done 1

--- src/app/modules/semesterRegistration/semesterRegistration.validation.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.validation.ts
@@ -1,15 +1,21 @@
 
 import { z } from "zod";
+import { semesterRegistrationStatus } from "./semesterRegistration.constant";
 
 const createSemesterRegistrationValidationSchema = z.object({
     body : z.object({
-
+        academicSemester : z.string() ,
+        status : z.enum([...semesterRegistrationStatus] as [string]) ,
+        startDate : z.string().datetime() ,
+        endDate : z.string().datetime() ,
+        minCredit : z.number() ,
+        maxCredit : z.number() ,
     }),
 })
 
 const updateSemesterRegistrationValidationSchema = z.object({
     body : z.object({
-
+        
     }),
 })
 
