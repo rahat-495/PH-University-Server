c88dc0cb3b7162b60c6885aa39065793f254cb16
rahat-495 <md8702687@gmail.com>
2025-01-09 21:55:59 +0600

    semeter registration service some things are updated at dist folder !

--- dist/app/modules/semesterRegistration/semesterRegistration.services.js
+++ dist/app/modules/semesterRegistration/semesterRegistration.services.js
@@ -15,9 +15,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.semesterRegistrationServices = void 0;
 const QueryBuilder_1 = __importDefault(require("../../builder/QueryBuilder"));
 const AppErrors_1 = __importDefault(require("../../errors/AppErrors"));
+const semesterRegistration_constant_1 = require("./semesterRegistration.constant");
 const semesterRegistration_model_1 = require("./semesterRegistration.model");
 const createSemesterRegistrationIntoDb = (payload) => __awaiter(void 0, void 0, void 0, function* () {
-    const isAnySemesterUpcomingOrOngoing = yield semesterRegistration_model_1.semesterRegistrationsModel.findOne({ $or: [{ status: "UPCOMING" }, { status: "ONGOING" }] });
+    const isAnySemesterUpcomingOrOngoing = yield semesterRegistration_model_1.semesterRegistrationsModel.findOne({ $or: [{ status: semesterRegistration_constant_1.registrationStatus.UPCOMING }, { status: semesterRegistration_constant_1.registrationStatus.ONGOING }] });
     if (isAnySemesterUpcomingOrOngoing) {
         throw new AppErrors_1.default(400, `There is already a ${isAnySemesterUpcomingOrOngoing.status} registrered semester !`);
     }
