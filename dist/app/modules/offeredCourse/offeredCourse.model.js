37faf70b8bb6e08c31036c25612d33544f7d3ec2
rahat-495 <md8702687@gmail.com>
2025-01-11 15:39:59 +0600

    offered Courses Model are created at dist folder !

--- dist/app/modules/offeredCourse/offeredCourse.model.js
+++ dist/app/modules/offeredCourse/offeredCourse.model.js
@@ -1,1 +1,6 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.offeredCoursesModel = void 0;
+const mongoose_1 = require("mongoose");
+const offeredCourseSchema = new mongoose_1.Schema({});
+exports.offeredCoursesModel = (0, mongoose_1.model)("offeredCourse", offeredCourseSchema);
