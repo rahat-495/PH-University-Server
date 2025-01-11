1ee5101159256de0212e8200d055d7a1fe020de6
rahat-495 <md8702687@gmail.com>
2025-01-11 15:41:49 +0600

    offered course router are created !

---
+++ src/app/modules/offeredCourse/offeredCourse.routes.ts
@@ -0,0 +1,11 @@
+
+import { Router } from "express";
+
+const router = Router() ;
+
+router.post('/') ;
+router.get('/') ;
+router.get('/') ;
+router.patch('/') ;
+
+export const offeredCourseRoutes = router ;
