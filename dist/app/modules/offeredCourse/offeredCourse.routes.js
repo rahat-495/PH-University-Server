23d75e9855bd795403ed571d54a1a9e1b1833a6d
rahat-495 <md8702687@gmail.com>
2025-01-11 15:42:01 +0600

    offered course router are created at dist folder !

--- dist/app/modules/offeredCourse/offeredCourse.routes.js
+++ dist/app/modules/offeredCourse/offeredCourse.routes.js
@@ -1,1 +1,10 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.offeredCourseRoutes = void 0;
+const express_1 = require("express");
+const router = (0, express_1.Router)();
+router.post('/');
+router.get('/');
+router.get('/');
+router.patch('/');
+exports.offeredCourseRoutes = router;
