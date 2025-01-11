d8e728a779af3e7cd3059de0b1c8d184571cc006
rahat-495 <md8702687@gmail.com>
2025-01-11 15:49:12 +0600

    offered course services are added at dist folder !

--- dist/app/modules/offeredCourse/offeredCourse.services.js
+++ dist/app/modules/offeredCourse/offeredCourse.services.js
@@ -1,1 +1,34 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.offeredCourseServices = void 0;
+const createOfferedCourseIntoDb = (payload) => __awaiter(void 0, void 0, void 0, function* () {
+    // const result = await coursesModel.create(payload) ;
+    // return result ;
+});
+const getAllOfferedCourseFromDb = (query) => __awaiter(void 0, void 0, void 0, function* () {
+    // const courseQuery = new QueryBuilder(coursesModel.find().populate("preRequisiteCourses.course") , query).search(courseSearchAbleFields).filter().sort().paginate().fields() ;
+    // const result = await courseQuery.modelQuery ;
+    // return result ;
+});
+const getSingleOfferedCourseFromDb = (id) => __awaiter(void 0, void 0, void 0, function* () {
+    // const result = await coursesModel.findById(id) ;
+    // return result ;
+});
+const updateOfferedCourseIntoDb = (id, payload) => __awaiter(void 0, void 0, void 0, function* () {
+    // const {preRequisiteCourses , ...courseRemainingData} = payload ;
+});
+exports.offeredCourseServices = {
+    createOfferedCourseIntoDb,
+    getAllOfferedCourseFromDb,
+    updateOfferedCourseIntoDb,
+    getSingleOfferedCourseFromDb,
+};
