7716fb47b2b0400a451648403baf6c97b6b6418e
rahat-495 <md8702687@gmail.com>
2025-01-13 15:35:34 +0600

    offered course route validation are added at dist folder !

--- dist/app/modules/offeredCourse/offeredCourse.routes.js
+++ dist/app/modules/offeredCourse/offeredCourse.routes.js
@@ -1,11 +1,16 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.offeredCourseRoutes = void 0;
 const express_1 = require("express");
 const offeredCourse_controllers_1 = require("./offeredCourse.controllers");
+const validateRequest_1 = __importDefault(require("../middlewares/validateRequest"));
+const offeredCourse_validations_1 = require("./offeredCourse.validations");
 const router = (0, express_1.Router)();
 router.get('/', offeredCourse_controllers_1.offeredCourseControllers.getAllOfferdCourses);
-router.post('/', offeredCourse_controllers_1.offeredCourseControllers.createOfferedCourse);
+router.post('/', (0, validateRequest_1.default)(offeredCourse_validations_1.offeredCourseValidations.createOfferedCourseValidation), offeredCourse_controllers_1.offeredCourseControllers.createOfferedCourse);
 router.patch('/:id', offeredCourse_controllers_1.offeredCourseControllers.updateOfferedCourse);
 router.get('/:id', offeredCourse_controllers_1.offeredCourseControllers.getSingleOfferedCourse);
 exports.offeredCourseRoutes = router;
