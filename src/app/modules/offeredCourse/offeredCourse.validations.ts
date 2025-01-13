5a95e5ef2477a7bad3fb5a5db7bc4580aad416c0
rahat-495 <md8702687@gmail.com>
2025-01-13 15:33:10 +0600

    offered course validation schema are done !

---
+++ src/app/modules/offeredCourse/offeredCourse.validations.ts
@@ -0,0 +1,19 @@
+
+import { z } from "zod" ;
+import { Days } from "./offeredCourse.constants";
+
+const createOfferedCourseValidation = z.object({
+    body : z.object({
+        semesterRegistration : z.string() ,
+        academicSemester : z.string() ,
+        academicFaculty : z.string() ,
+        academicDepartment : z.string() ,
+        course : z.string() ,
+        faculty : z.string() ,
+        maxCapacity : z.number() ,
+        section : z.number() ,
+        days : z.enum([...Days] as [string]) ,
+        startTime : z.string() ,
+        endTime : z.string() ,
+    })
+})
