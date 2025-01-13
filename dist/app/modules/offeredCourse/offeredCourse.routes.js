00f0530bdb527ab703154e6de0d6fa8b8df36348
rahat-495 <md8702687@gmail.com>
2025-01-13 15:46:20 +0600

    creating offered course route name are changed at dist folder !

--- dist/app/modules/offeredCourse/offeredCourse.routes.js
+++ dist/app/modules/offeredCourse/offeredCourse.routes.js
@@ -10,7 +10,7 @@ const validateRequest_1 = __importDefault(require("../middlewares/validateReques
 const offeredCourse_validations_1 = require("./offeredCourse.validations");
 const router = (0, express_1.Router)();
 router.get('/', offeredCourse_controllers_1.offeredCourseControllers.getAllOfferdCourses);
-router.post('/', (0, validateRequest_1.default)(offeredCourse_validations_1.offeredCourseValidations.createOfferedCourseValidation), offeredCourse_controllers_1.offeredCourseControllers.createOfferedCourse);
 router.patch('/:id', offeredCourse_controllers_1.offeredCourseControllers.updateOfferedCourse);
 router.get('/:id', offeredCourse_controllers_1.offeredCourseControllers.getSingleOfferedCourse);
+router.post('/create-offered-course', (0, validateRequest_1.default)(offeredCourse_validations_1.offeredCourseValidations.createOfferedCourseValidation), offeredCourse_controllers_1.offeredCourseControllers.createOfferedCourse);
 exports.offeredCourseRoutes = router;
