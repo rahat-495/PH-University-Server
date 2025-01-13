01f82cc3964547e22be1e9fe119abf00b7d60ccd
rahat-495 <md8702687@gmail.com>
2025-01-13 15:33:29 +0600

    offered course validation schema are done at dist folder!

--- dist/app/modules/offeredCourse/offeredCourse.validations.js
+++ dist/app/modules/offeredCourse/offeredCourse.validations.js
@@ -1,1 +1,19 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const zod_1 = require("zod");
+const offeredCourse_constants_1 = require("./offeredCourse.constants");
+const createOfferedCourseValidation = zod_1.z.object({
+    body: zod_1.z.object({
+        semesterRegistration: zod_1.z.string(),
+        academicSemester: zod_1.z.string(),
+        academicFaculty: zod_1.z.string(),
+        academicDepartment: zod_1.z.string(),
+        course: zod_1.z.string(),
+        faculty: zod_1.z.string(),
+        maxCapacity: zod_1.z.number(),
+        section: zod_1.z.number(),
+        days: zod_1.z.enum([...offeredCourse_constants_1.Days]),
+        startTime: zod_1.z.string(),
+        endTime: zod_1.z.string(),
+    })
+});
