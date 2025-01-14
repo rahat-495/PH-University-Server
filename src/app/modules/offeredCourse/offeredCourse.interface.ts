c620bc64980aa2982c112f82cf77cd25d2459620
rahat-495 <md8702687@gmail.com>
2025-01-14 21:25:14 +0600

    offered course inter face are updated !

--- src/app/modules/offeredCourse/offeredCourse.interface.ts
+++ src/app/modules/offeredCourse/offeredCourse.interface.ts
@@ -12,7 +12,7 @@ export type TOfferedCourse = {
     faculty : Types.ObjectId ;
     maxCapacity : number ;
     section : number ;
-    days : TDays ;
+    days : TDays[] ;
     startTime : string ;
     endTime : string ;
 }
