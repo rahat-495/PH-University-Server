b5e1c9039fd2d741c722a8eb08755a6a4e994016
rahat-495 <md8702687@gmail.com>
2025-01-15 14:31:54 +0600

    creating offered course service are done !

--- src/app/modules/offeredCourse/offeredCourse.services.ts
+++ src/app/modules/offeredCourse/offeredCourse.services.ts
@@ -1,10 +1,43 @@
 
+import AppError from "../../errors/AppErrors";
+import { academicDepartmentsModel } from "../academicDepartment/academicDepartment.model";
+import { academicFacultysModel } from "../academicFaculty/academicFaculty.model";
+import { coursesModel } from "../course/course.model";
+import { facultysModel } from "../faculty/faculty.model";
+import { semesterRegistrationsModel } from "../semesterRegistration/semesterRegistration.model";
 import { TOfferedCourse } from "./offeredCourse.interface"
 import { offeredCoursesModel } from "./offeredCourse.model";
 
 const createOfferedCourseIntoDb = async (payload: TOfferedCourse) => {
-    // Other ObjectId validation are working on model pre hook -------
-    const result = await offeredCoursesModel.create(payload) ;
+    
+    const {academicDepartment , academicFaculty , course , faculty , semesterRegistration } = payload ;
+    
+    const isAcademicDepartmentAxist = await academicDepartmentsModel.findById(academicDepartment) ;
+    if(!isAcademicDepartmentAxist){
+        throw new AppError(404 , "Academic Department are not found !") ;
+    }
+    
+    const isAcademicFacultyAxist = await academicFacultysModel.findById(academicFaculty) ;
+    if(!isAcademicFacultyAxist){
+        throw new AppError(404 , "Academic Faculty are not found !") ;
+    }
+    
+    const isSemesterRegistrationAxist = await semesterRegistrationsModel.findById(semesterRegistration) ;
+    if(!isSemesterRegistrationAxist){
+        throw new AppError(404 , "Semester Registration are not found !") ;
+    }
+    
+    const isCourseAxist = await coursesModel.findById(course) ;
+    if(!isCourseAxist){
+        throw new AppError(404 , "Course are not found !") ;
+    }
+    
+    const isFacultyAxist = await facultysModel.findById(faculty) ;
+    if(!isFacultyAxist){
+        throw new AppError(404 , "Faculty are not found !") ;
+    }
+
+    const result = await offeredCoursesModel.create({...payload , academicSemester : isSemesterRegistrationAxist?.academicSemester}) ;
     return result ;
 }
 
