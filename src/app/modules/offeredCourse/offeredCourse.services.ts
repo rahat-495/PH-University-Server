b18c92b8314e40274633016f250f96b531743b29
rahat-495 <md8702687@gmail.com>
2025-01-15 15:45:19 +0600

    some things are now organized !

--- src/app/modules/offeredCourse/offeredCourse.services.ts
+++ src/app/modules/offeredCourse/offeredCourse.services.ts
@@ -7,6 +7,7 @@ import { facultysModel } from "../faculty/faculty.model";
 import { semesterRegistrationsModel } from "../semesterRegistration/semesterRegistration.model";
 import { TOfferedCourse } from "./offeredCourse.interface"
 import { offeredCoursesModel } from "./offeredCourse.model";
+import { hasTimeConflict } from "./offeredCourse.utils";
 
 const createOfferedCourseIntoDb = async (payload: TOfferedCourse) => {
     
@@ -51,16 +52,9 @@ const createOfferedCourseIntoDb = async (payload: TOfferedCourse) => {
     
     const assignedSchedules = await offeredCoursesModel.find({faculty , semesterRegistration , days : { $in : days}}).select("startTime endTime days") ;
     
-    assignedSchedules.forEach((schedules) => {
-        const existingStartTime = new Date(`2007-03-05T${schedules?.startTime}:00`) ;
-        const existingEndTime = new Date(`2007-03-05T${schedules?.endTime}:00`) ;
-        const newStartTime = new Date(`2007-03-05T${newSchedule?.startTime}:00`) ;
-        const newEndTime = new Date(`2007-03-05T${newSchedule?.endTime}:00`) ;
-        
-        if(newStartTime < existingEndTime && newEndTime > existingStartTime){
-            throw new AppError(409 , `This faculty is not available at this time . Choose anothor time or day !`) ;
-        }
-    })
+    if(hasTimeConflict(assignedSchedules , newSchedule)){
+        throw new AppError(409 , `This faculty is not available at this time . Choose anothor time or day !`) ;
+    }
     
     const result = await offeredCoursesModel.create({...payload , academicSemester : isSemesterRegistrationAxist?.academicSemester}) ;
     return result ;
--- src/app/modules/offeredCourse/offeredCourse.utils.ts
+++ src/app/modules/offeredCourse/offeredCourse.utils.ts
@@ -1,2 +1,21 @@
+import { TDays } from "./offeredCourse.interface"
 
+type TSchedules = {
+    days : TDays[] ;
+    endTime : string ;
+    startTime : string ;
+}
 
+export const hasTimeConflict = (assignedSchedules : TSchedules[] , newSchedule : TSchedules) => {
+    for(const schedule of assignedSchedules){
+        const existingStartTime = new Date(`2007-03-05T${schedule?.startTime}:00`) ;
+        const existingEndTime = new Date(`2007-03-05T${schedule?.endTime}:00`) ;
+        const newStartTime = new Date(`2007-03-05T${newSchedule?.startTime}:00`) ;
+        const newEndTime = new Date(`2007-03-05T${newSchedule?.endTime}:00`) ;
+        
+        if(newStartTime < existingEndTime && newEndTime > existingStartTime){
+            return true ;
+        }
+    }
+    return false ;
+}
