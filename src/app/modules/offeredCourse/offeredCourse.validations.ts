7aa1e2859903cd1fb426eb0f26d0e25d9735fa4a
rahat-495 <md8702687@gmail.com>
2025-01-16 15:36:47 +0600

    offered course validation schema are updated !

--- src/app/modules/offeredCourse/offeredCourse.validations.ts
+++ src/app/modules/offeredCourse/offeredCourse.validations.ts
@@ -35,11 +35,27 @@ const createOfferedCourseValidation = z.object({
 
 const updateOfferedCourseValidation = z.object({
     body : z.object({
-        faculty : z.string().optional() ,
-        days : z.enum([...Days] as [string]).optional() ,
-        maxCapacity : z.number().optional() ,
-        startTime : z.string().optional() ,
-        endTime : z.string().optional() ,
+        faculty : z.string() ,
+        days : z.array(z.enum([...Days] as [string])) ,
+        maxCapacity : z.number() ,
+        startTime : z.string().refine((time) => {
+            const regex = /^(?:[01]\d|2[0-3])\s*:\s*[0-5]\d$/ ;
+            return regex.test(time) ;
+        },{ 
+            message : "Invalid Time Format Expected 'HH:MM' !" ,
+        }) ,
+        endTime : z.string().refine((time) => {
+            const regex = /^(?:[01]\d|2[0-3])\s*:\s*[0-5]\d$/ ;
+            return regex.test(time) ;
+        },{ 
+            message : "Invalid Time Format Expected 'HH:MM' !"
+        }) ,
+    }).refine((body) => {
+        const start = new Date(`2007-03-05T${body.startTime}:00`) ;
+        const end = new Date(`2007-03-05T${body.endTime}:00`) ;
+        return end > start ;
+    },{
+        message : `Start time should be before End Time !`
     })
 });
 
