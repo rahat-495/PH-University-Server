0d67563464895d45ba47417cbe1cdeae13e7cf9a
rahat-495 <md8702687@gmail.com>
2025-01-16 15:37:04 +0600

    offered course validation schema are updated at dist folder !

--- dist/app/modules/offeredCourse/offeredCourse.validations.js
+++ dist/app/modules/offeredCourse/offeredCourse.validations.js
@@ -35,11 +35,27 @@ const createOfferedCourseValidation = zod_1.z.object({
 });
 const updateOfferedCourseValidation = zod_1.z.object({
     body: zod_1.z.object({
-        faculty: zod_1.z.string().optional(),
-        days: zod_1.z.enum([...offeredCourse_constants_1.Days]).optional(),
-        maxCapacity: zod_1.z.number().optional(),
-        startTime: zod_1.z.string().optional(),
-        endTime: zod_1.z.string().optional(),
+        faculty: zod_1.z.string(),
+        days: zod_1.z.array(zod_1.z.enum([...offeredCourse_constants_1.Days])),
+        maxCapacity: zod_1.z.number(),
+        startTime: zod_1.z.string().refine((time) => {
+            const regex = /^(?:[01]\d|2[0-3])\s*:\s*[0-5]\d$/;
+            return regex.test(time);
+        }, {
+            message: "Invalid Time Format Expected 'HH:MM' !",
+        }),
+        endTime: zod_1.z.string().refine((time) => {
+            const regex = /^(?:[01]\d|2[0-3])\s*:\s*[0-5]\d$/;
+            return regex.test(time);
+        }, {
+            message: "Invalid Time Format Expected 'HH:MM' !"
+        }),
+    }).refine((body) => {
+        const start = new Date(`2007-03-05T${body.startTime}:00`);
+        const end = new Date(`2007-03-05T${body.endTime}:00`);
+        return end > start;
+    }, {
+        message: `Start time should be before End Time !`
     })
 });
 exports.offeredCourseValidations = {
