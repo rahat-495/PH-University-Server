8c87031fbf523faa58d60ed75731ef8ec83651c9
rahat-495 <md8702687@gmail.com>
2025-01-18 21:33:09 +0600

    auth service ar done !

--- src/app/modules/auth/auth.controllers.ts
+++ src/app/modules/auth/auth.controllers.ts
@@ -1,8 +1,14 @@
 
 import catchAsync from "../../utils/catchAsync"
+import sendResponse from "../../utils/sendResponse";
+import { authServices } from "./auth.services";
 
 const loginUser = catchAsync(async (req , res) => {
-
+    const result = await authServices.loginUser(req.body) ;
+    if(result){
+        sendResponse
+        <object>(res , {data : result , statusCode : 200 , success : true , message : "User Login Successfully !"}) ;
+    }
 })
 
 export const authControllers = {
---
+++ src/app/modules/auth/auth.services.ts
@@ -0,0 +1,12 @@
+
+import { TLoginUser } from "./auth.interface"
+
+const loginUser = async (payload : TLoginUser) => {
+    
+
+    return null ;
+}
+
+export const authServices = {
+    loginUser ,
+}
