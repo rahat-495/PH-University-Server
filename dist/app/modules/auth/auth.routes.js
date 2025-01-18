da0310a38a0b60fcee2fc5f7bd378cfa93c46aa4
rahat-495 <md8702687@gmail.com>
2025-01-18 21:28:20 +0600

    auth routes are done at dist folder !

--- dist/app/modules/auth/auth.routes.js
+++ dist/app/modules/auth/auth.routes.js
@@ -1,1 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.authRoutes = void 0;
+const express_1 = require("express");
+const validateRequest_1 = __importDefault(require("../middlewares/validateRequest"));
+const auth_validation_1 = require("./auth.validation");
+const router = (0, express_1.Router)();
+router.post('/login', (0, validateRequest_1.default)(auth_validation_1.authValidations.loginValidationSchema));
+exports.authRoutes = router;
