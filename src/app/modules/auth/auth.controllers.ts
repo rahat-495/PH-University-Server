65087deddc8050e46009a6118e2b46f67840689c
rahat-495 <md8702687@gmail.com>
2025-01-18 21:29:52 +0600

    auth login route controller are added !

---
+++ src/app/modules/auth/auth.controllers.ts
@@ -0,0 +1,10 @@
+
+import catchAsync from "../../utils/catchAsync"
+
+const loginUser = catchAsync(async (req , res) => {
+
+})
+
+export const authControllers = {
+    loginUser ,
+}
--- src/app/modules/auth/auth.routes.ts
+++ src/app/modules/auth/auth.routes.ts
@@ -2,9 +2,10 @@
 import { Router } from "express";
 import validateRequest from "../middlewares/validateRequest";
 import { authValidations } from "./auth.validation";
+import { authControllers } from "./auth.controllers";
 
 const router = Router() ;
 
-router.post('/login' , validateRequest(authValidations.loginValidationSchema)) ;
+router.post('/login' , validateRequest(authValidations.loginValidationSchema) , authControllers.loginUser) ;
 
 export const authRoutes = router ;
