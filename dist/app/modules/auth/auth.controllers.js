b9da417dc340786c8a657111f758660798615221
rahat-495 <md8702687@gmail.com>
2025-01-18 21:33:21 +0600

    auth service ar done at dist folder !

--- dist/app/modules/auth/auth.controllers.js
+++ dist/app/modules/auth/auth.controllers.js
@@ -14,7 +14,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.authControllers = void 0;
 const catchAsync_1 = __importDefault(require("../../utils/catchAsync"));
+const sendResponse_1 = __importDefault(require("../../utils/sendResponse"));
+const auth_services_1 = require("./auth.services");
 const loginUser = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield auth_services_1.authServices.loginUser(req.body);
+    if (result) {
+        (0, sendResponse_1.default)(res, { data: result, statusCode: 200, success: true, message: "User Login Successfully !" });
+    }
 }));
 exports.authControllers = {
     loginUser,
