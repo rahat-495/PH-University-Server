01ad9666b7041765b57b013d84448e188db217a9
rahat-495 <md8702687@gmail.com>
2025-01-18 21:25:19 +0600

    auth validation are done at dist folder !

--- dist/app/modules/auth/auth.validation.js
+++ dist/app/modules/auth/auth.validation.js
@@ -1,1 +1,13 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.authValidations = void 0;
+const zod_1 = require("zod");
+const loginValidationSchema = zod_1.z.object({
+    body: zod_1.z.object({
+        id: zod_1.z.string({ required_error: "Id is required !" }),
+        password: zod_1.z.string({ required_error: "Password is required !" }),
+    })
+});
+exports.authValidations = {
+    loginValidationSchema,
+};
