d96c7210b1a609261571ba26624e3cdaae14daa3
rahat-495 <md8702687@gmail.com>
2025-01-20 15:31:11 +0600

    auth verification are done at dist folder !

---
+++ dist/app/modules/middlewares/auth.js
@@ -0,0 +1,35 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const catchAsync_1 = __importDefault(require("../../utils/catchAsync"));
+const AppErrors_1 = __importDefault(require("../../errors/AppErrors"));
+const http_status_codes_1 = __importDefault(require("http-status-codes"));
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const config_1 = __importDefault(require("../../config"));
+const auth = () => {
+    return (0, catchAsync_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+        const token = req.headers.authorization;
+        if (!token) {
+            throw new AppErrors_1.default(http_status_codes_1.default.UNAUTHORIZED, "You are not authorized !");
+        }
+        jsonwebtoken_1.default.verify(token, config_1.default.jwtAccessSecret, function (err, decoded) {
+            if (err) {
+                throw new AppErrors_1.default(http_status_codes_1.default.UNAUTHORIZED, "You are not authorized !");
+            }
+            req.user = decoded;
+        });
+        next();
+    }));
+};
+exports.default = auth;
