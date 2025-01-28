9331faab84e7850ee4813a49707775769ff9e653
rahat-495 <md8702687@gmail.com>
2025-01-28 15:48:52 +0600

    cookie parser are installed at dist folder !

--- dist/app.js
+++ dist/app.js
@@ -17,9 +17,11 @@ const cors_1 = __importDefault(require("cors"));
 const globalErrorHandler_1 = __importDefault(require("./app/modules/middlewares/globalErrorHandler"));
 const notFound_1 = __importDefault(require("./app/modules/middlewares/notFound"));
 const routes_1 = __importDefault(require("./app/routes"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)());
 app.use(express_1.default.json());
+app.use((0, cookie_parser_1.default)());
+app.use((0, cors_1.default)({ origin: ['http://localhost:5173'], credentials: true }));
 app.use('/api/v1', routes_1.default);
 app.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     res.json({ message: "The second project server are running !", success: true });
