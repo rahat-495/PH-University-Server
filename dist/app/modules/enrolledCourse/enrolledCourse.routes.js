230e93a2a803b38677194714d00c859440d1992b
rahat-495 <md8702687@gmail.com>
2025-02-05 11:24:36 +0600

    enrolledCourse routes are created at dist folder !

--- dist/app/modules/enrolledCourse/enrolledCourse.routes.js
+++ dist/app/modules/enrolledCourse/enrolledCourse.routes.js
@@ -1,1 +1,6 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.enrolledCourseRoutes = void 0;
+const express_1 = require("express");
+const router = (0, express_1.Router)();
+exports.enrolledCourseRoutes = router;
