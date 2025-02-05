bf03e2b0478129723ab22b764261221394f1582d
rahat-495 <md8702687@gmail.com>
2025-02-05 11:23:51 +0600

    enrolledCourse validation schema are done at dist folder !

--- dist/app/modules/enrolledCourse/enrolledCourse.validations.js
+++ dist/app/modules/enrolledCourse/enrolledCourse.validations.js
@@ -1,1 +1,10 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.enrolledCourseValidations = void 0;
+const zod_1 = require("zod");
+const createEnrolledCourseValidationSchema = zod_1.z.object({
+    body: zod_1.z.object({ offeredCouse: zod_1.z.string() })
+});
+exports.enrolledCourseValidations = {
+    createEnrolledCourseValidationSchema,
+};
