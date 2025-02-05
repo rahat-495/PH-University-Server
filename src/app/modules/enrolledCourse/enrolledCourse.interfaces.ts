3318611fab8e45e9a2f8c646c6bbe7b9dd1c658f
rahat-495 <md8702687@gmail.com>
2025-02-05 11:05:09 +0600

    enrolledCourse interfaces are done !

---
+++ src/app/modules/enrolledCourse/enrolledCourse.interfaces.ts
@@ -0,0 +1,26 @@
+
+import { Types } from "mongoose";
+
+export type courseMarks = {
+    classTest1 : number ;
+    midTerm : number ;
+    classTest2 : number ;
+    finelTerm : number ;
+}
+
+export type TGrade = "A" | "B" | "C" | "D" | "F" | "NA" ;
+
+export type TEnrolledCourse = {
+    semesterRegistration : Types.ObjectId ;
+    academicSemester ?: Types.ObjectId ;
+    academicFaculty : Types.ObjectId ;
+    academicDepartment : Types.ObjectId ;
+    course : Types.ObjectId ;
+    student : Types.ObjectId ;
+    faculty : Types.ObjectId ;
+    isEnrolled : boolean ;
+    courseMarks : courseMarks ;
+    grade : TGrade;
+    gradePoints : number ;
+    isCompleted : boolean ;
+}
